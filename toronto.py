4b3298a4598599722ec8fd078c5ce96cc73d5e0c
Tungdam <Tungdamx@gmail.com>
2019-05-20 06:29:06 +0700

    remove content table. Add long - lat to dataframe

---
+++ toronto.py
@@ -0,0 +1,98 @@
+#!/usr/bin/env python3
+from bs4 import BeautifulSoup
+import requests
+import pandas as pd
+
+with open('/tmp/canada_postal_code.xml') as f:
+     soup=BeautifulSoup(f,'html.parser')
+
+L=[]
+for i in range(1,len(soup.table.find_all('tr'))):
+    L.append([ x.rstrip('\n') for x in soup.table.find_all('tr')[i].strings if x.rstrip('\n') != '' ])
+
+columns=['PostCode','Borough','Neighborhood']
+df=pd.DataFrame(L,columns=columns)
+#print(df.head())
+
+#Drop rows with Borough == 'Not assigned'
+df=df[df['Borough'] != 'Not assigned']
+
+#Group multile neighbors into 1 row
+df=df.groupby(['PostCode','Borough'],as_index=True)['Neighborhood'].apply(', '.join).reset_index()
+
+#Replace 'Not assigned' Neighborhood by Borough
+df.Neighborhood[df.Neighborhood == 'Not assigned'] = df.Borough
+
+print(df.head())
+
+#Load lat long
+lat_long=pd.read_csv('Geospatial_data')
+
+#Update column name
+lat_long.rename(columns={'Postal Code':'PostCode'},inplace=True)
+print(lat_long.head())
+
+#Merge to DataFrame
+df=pd.merge(df,lat_long,on='PostCode')
+
+#Take data with borough ~ 'Toronto' on
+toronto_data=df[df['Borough'].str.find("Toronto") > -1].reset_index()
+print(toronto_data.head())
+######
+CLIENT_ID = 'NOCPT3ULRVCEGUZIFDUDREID0FCWL4NCTTLTUZM4BNLSMW5C' # your Foursquare ID
+CLIENT_SECRET = 'Q3OUHQKP3JL5UPEJCVFBC1G0M1DVGUID1RINFAFOPELUYV2F' # your Foursquare Secret
+VERSION = '20190506' # Foursquare API version
+
+print('Your credentails:')
+print('CLIENT_ID: ' + CLIENT_ID)
+print('CLIENT_SECRET:' + CLIENT_SECRET)
+
+######
+# type your answer here
+LIMIT=100
+radius=500
+def getNearbyVenues(names, latitudes, longitudes, radius=500):
+    
+    venues_list=[]
+    for name, lat, lng in zip(names, latitudes, longitudes):
+        print(name)
+            
+        # create the API request URL
+        url = 'https://api.foursquare.com/v2/venues/explore?&client_id={}&client_secret={}&v={}&ll={},{}&radius={}&limit={}'.format(
+            CLIENT_ID, 
+            CLIENT_SECRET, 
+            VERSION, 
+            lat, 
+            lng, 
+            radius, 
+            LIMIT)
+            
+        # make the GET request
+        results = requests.get(url).json()["response"]['groups'][0]['items']
+        
+        # return only relevant information for each nearby venue
+        venues_list.append([(
+            name, 
+            lat, 
+            lng, 
+            v['venue']['name'], 
+            v['venue']['location']['lat'], 
+            v['venue']['location']['lng'],  
+            v['venue']['categories'][0]['name']) for v in results])
+
+    nearby_venues = pd.DataFrame([item for venue_list in venues_list for item in venue_list])
+    nearby_venues.columns = ['Neighborhood', 
+                  'Neighborhood Latitude', 
+                  'Neighborhood Longitude', 
+                  'Venue', 
+                  'Venue Latitude', 
+                  'Venue Longitude', 
+                  'Venue Category']
+    
+    return(nearby_venues)
+
+toronto_venues = getNearbyVenues(names=toronto_data['Neighborhood'],
+                                   latitudes=toronto_data['Latitude'],
+                                   longitudes=toronto_data['Longitude']
+                                  )
+print(toronto_venues)
