75f605a7106dc6d4ee262d4f053efab7afd11b83
Tungdam <Tungdamx@gmail.com>
2019-05-20 09:56:29 +0700

    Final parts: get venues by foursquareAPI, cluster and visualize by folium

--- toronto.py
+++ toronto.py
@@ -96,3 +96,29 @@ toronto_venues = getNearbyVenues(names=toronto_data['Neighborhood'],
                                    longitudes=toronto_data['Longitude']
                                   )
 print(toronto_venues)
+
+def return_most_common_venues(row, num_top_venues):
+    row_categories = row.iloc[1:]
+    row_categories_sorted = row_categories.sort_values(ascending=False)
+    return row_categories_sorted.index.values[0:num_top_venues]
+
+
+num_top_venues=10
+indicators = ['st','nd','rd']
+
+#Create columns according to number of top ve
+columns = ['Neighborhood']
+for ind in np.arange(num_top_venues):
+    try:
+        columns.append('{}{} Most Common Venue'.format(ind+1,indicators[ind]))
+    except:
+        columns.append('{}th Most Common Venue'.format(ind+1))
+
+#Create a new da
+neighborhoods_venues_sorted = pd.DataFrame(columns=columns)
+neighborhoods_venues_sorted['Neighborhood'] = toronto_grouped['Neighborhood']
+
+for ind in np.arange(toronto_grouped.shape[0]):
+    neighborhoods_venues_sorted.iloc[ind,1:] = return_most_common_venues(toronto_grouped.iloc[ind,:], num_top_venues)
+
+neighborhoods_venues_sorted.head()
