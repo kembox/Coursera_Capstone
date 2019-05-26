3e472e8ac4b5b468a7507b2f498a4ecb47094d8a
Tungdam <Tungdamx@gmail.com>
2019-05-26 14:26:08 +0700

    new file for final assignment

--- toronto.py
+++ toronto.py
@@ -2,8 +2,9 @@
 from bs4 import BeautifulSoup
 import requests
 import pandas as pd
+import foursquare
 
-with open('/tmp/canada_postal_code.xml') as f:
+with open('canada_postal_code.xml') as f:
      soup=BeautifulSoup(f,'html.parser')
 
 L=[]
@@ -47,78 +48,3 @@ print('Your credentails:')
 print('CLIENT_ID: ' + CLIENT_ID)
 print('CLIENT_SECRET:' + CLIENT_SECRET)
 
-######
-# type your answer here
-LIMIT=100
-radius=500
-def getNearbyVenues(names, latitudes, longitudes, radius=500):
-    
-    venues_list=[]
-    for name, lat, lng in zip(names, latitudes, longitudes):
-        print(name)
-            
-        # create the API request URL
-        url = 'https://api.foursquare.com/v2/venues/explore?&client_id={}&client_secret={}&v={}&ll={},{}&radius={}&limit={}'.format(
-            CLIENT_ID, 
-            CLIENT_SECRET, 
-            VERSION, 
-            lat, 
-            lng, 
-            radius, 
-            LIMIT)
-            
-        # make the GET request
-        results = requests.get(url).json()["response"]['groups'][0]['items']
-        
-        # return only relevant information for each nearby venue
-        venues_list.append([(
-            name, 
-            lat, 
-            lng, 
-            v['venue']['name'], 
-            v['venue']['location']['lat'], 
-            v['venue']['location']['lng'],  
-            v['venue']['categories'][0]['name']) for v in results])
-
-    nearby_venues = pd.DataFrame([item for venue_list in venues_list for item in venue_list])
-    nearby_venues.columns = ['Neighborhood', 
-                  'Neighborhood Latitude', 
-                  'Neighborhood Longitude', 
-                  'Venue', 
-                  'Venue Latitude', 
-                  'Venue Longitude', 
-                  'Venue Category']
-    
-    return(nearby_venues)
-
-toronto_venues = getNearbyVenues(names=toronto_data['Neighborhood'],
-                                   latitudes=toronto_data['Latitude'],
-                                   longitudes=toronto_data['Longitude']
-                                  )
-print(toronto_venues)
-
-def return_most_common_venues(row, num_top_venues):
-    row_categories = row.iloc[1:]
-    row_categories_sorted = row_categories.sort_values(ascending=False)
-    return row_categories_sorted.index.values[0:num_top_venues]
-
-
-num_top_venues=10
-indicators = ['st','nd','rd']
-
-#Create columns according to number of top ve
-columns = ['Neighborhood']
-for ind in np.arange(num_top_venues):
-    try:
-        columns.append('{}{} Most Common Venue'.format(ind+1,indicators[ind]))
-    except:
-        columns.append('{}th Most Common Venue'.format(ind+1))
-
-#Create a new da
-neighborhoods_venues_sorted = pd.DataFrame(columns=columns)
-neighborhoods_venues_sorted['Neighborhood'] = toronto_grouped['Neighborhood']
-
-for ind in np.arange(toronto_grouped.shape[0]):
-    neighborhoods_venues_sorted.iloc[ind,1:] = return_most_common_venues(toronto_grouped.iloc[ind,:], num_top_venues)
-
-neighborhoods_venues_sorted.head()
